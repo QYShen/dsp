9bcaf2c8fd8aa021920dd5fe872c7e3f166b2aca
QYShen <shenqiuyun2228@hotmail.com>
2017-12-05 19:49:12 -0800

    Add files via upload

---
+++ editors/vinegar.py
@@ -0,0 +1,5 @@
+print("This file was created using the vim editor")
+print("This file was created by Qiuyun Shen")
+Need input "i" to change the command mode to insert mode.
+Whatever you want, and so on...
+
